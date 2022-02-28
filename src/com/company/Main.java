18c681a640e0e839bef06f08e9854ceee20cd155
Константин <znv5959@gmail.com>
2022-02-28 12:19:39 +0500

    Add if/else statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,17 @@ public class Main {
     public static void main(String[] args) {
 
         int[] price = new int[]{5, 100, 20, 66, 16};
-        int discount = 23;
+        int discount = 50;
         int offset = 1;
         int readLength = 3;
 
+
         Sale bot = new Sale();
-        System.out.println(Arrays.toString(bot.decryptData(price, discount, offset, readLength)));
+
+        if(discount >= 1 && discount <= 99  && readLength <= (price.length - offset) && offset > 0) {
+            System.out.println(Arrays.toString(bot.decryptData(price, discount, offset, readLength)));
+        } else System.out.println("Введенные значения не соответствуют условиям \n Скидка от 1% до 90% \n Offset должен быть положительным \n ReadLength не должен привышать длинну массива");
+
 
 
     }
--- src/com/company/Sale.java
+++ src/com/company/Sale.java
@@ -8,22 +8,23 @@ import java.util.Arrays;
 public class Sale {
 
 
-    public @Nullable float[] decryptData(@NonNull int[] price,
+    public @Nullable int[] decryptData(@NonNull int[] price,
                                        @IntRange(from = 1) int discount,
                                        @IntRange(from = 0) int offset,
                                        @IntRange(from = 1) int readLength) {
 
 
 
-        float[] newArr = new float[readLength];
+        int[] newArr = new int[readLength];
         int j = 0;
         for(int i = offset; i <= offset + readLength - 1 ; i++) {
-            newArr[j] = (float) price[i] - ((float) price[i] * ((float) discount / 100));
+            newArr[j] = (int) Math.floor((float) price[i] - ((float) price[i] * ((float) discount / 100)));
             j++;
         }
 
 
         return newArr;
+
     }
 
 
